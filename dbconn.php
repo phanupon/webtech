be94bb7a055721bcff425b7a55760c0709bfe305
phanupon <phanupon@192.168.2.7>
2014-06-02 19:46:19 +0700

    dbconfig edit Signed-off-by:phanupon <phanupon@192.168.2.7>

--- dbconn.php
+++ dbconn.php
@@ -1,5 +1,7 @@
 <?php
 //$host = "localhost";
+$OPENSHIFT_MYSQL_DB_PORT = "3306";
+$OPENSHIFT_MYSQL_DB_HOST = "127.12.70.2";
 $host = "mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/";
 $user = "adminjyfeVvm";
 $pass = "8HMVbQwR1Pxs";
