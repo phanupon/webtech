c589aa4bea339deec48e044583abaeaee9bbc855
phanupon <root@phanupons-MacBook-Pro.local>
2014-06-02 17:08:12 +0700

    Chang connect database.

--- dbconn.php
+++ dbconn.php
@@ -1,5 +1,6 @@
 <?php
-$host = "localhost";
+//$host = "localhost";
+$host = "mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/";
 $user = "adminjyfeVvm";
 $pass = "8HMVbQwR1Pxs";
 $conn = mysql_connect($host, $user, $pass);
